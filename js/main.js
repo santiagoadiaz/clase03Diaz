e0f9ed96f9a959d272740fd6f09e4ca2066e70cb
Santiago Diaz <santidiaz01@gmail.com>
2022-07-14 21:54:49 -0300

    inscripcion de equipo de futbol

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// inscripcion de equipo de futbol
+
+for (let i = 1; i < 12; i++) {
+
+    let ingresarNombre = prompt("Ingresar nombre del jugador");
+
+    alert(" Jugador  N° " + i + " Nombre: " + ingresarNombre);
+    console.log(i + ingresarNombre);
+}
